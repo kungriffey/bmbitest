a10d10a8843c23a740989b95ac7eb42def12e78c
kunwardeep <kunwardeep.gill@gmail.com>
2013-11-09 22:36:47 -0500

    a
    a

---
+++ project/League Wizard/leaguewizard.js
@@ -0,0 +1,20 @@
+<script type="text/javascript">
+        var next = 1;
+        function addFormField(){
+            var addto = "#field" + next;
+	        next = next + 1;
+            var newIn = '<br /><br /><input autocomplete="off" class="span3" id="field' + next + '" name="field' + next + '" type="text" data-provide="typeahead" data-items="8">';
+            var newInput = $(newIn);
+	        $(addto).after(newInput);
+            $("#field" + next).attr('data-source',$(addto).attr('data-source'));
+            $("#count").val(next);  
+        }
+
+        $(function() {
+    $('#registrationDeadlineTimePicker2').datetimepicker({
+      language: 'en',
+      pick12HourFormat: true
+    });
+  });
+        
+</script>
